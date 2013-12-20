2f22d713706e209d7b5923acb1da681f2a3e03d5
Sean OMeara <someara@opscode.com>
2013-12-20 14:22:59 -0500

    fixing up for rubocop

--- resources/package.rb
+++ resources/package.rb
@@ -21,11 +21,11 @@ actions :install
 attribute :app, :kind_of => String, :name_attribute => true
 attribute :source, :kind_of => String, :default => nil
 attribute :owner, :kind_of => String, :default => nil
-attribute :destination, :kind_of => String, :default => "/Applications"
+attribute :destination, :kind_of => String, :default => '/Applications'
 attribute :checksum, :kind_of => String, :default => nil
 attribute :volumes_dir, :kind_of => String, :default => nil
 attribute :dmg_name, :kind_of => String, :default => nil
-attribute :type, :kind_of => String, :default => "app"
+attribute :type, :kind_of => String, :default => 'app'
 attribute :installed, :kind_of => [TrueClass, FalseClass], :default => false
 attribute :package_id, :kind_of => String, :default => nil
 attribute :dmg_passphrase, :kind_of => String, :default => nil
