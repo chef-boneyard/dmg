edece035aee29a4a9df4f1e3d275260d40d4bd86
Tim Smith <tsmith@chef.io>
2017-03-21 15:52:56 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'dmg'
 maintainer       'Chef Software, Inc'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'LWRP to install OS X applications from dmgs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.1.0'
