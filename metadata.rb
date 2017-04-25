7641292f74773ee1a2305babb60a98b198fb01cd
Tim Smith <tsmith@chef.io>
2017-04-24 23:32:17 -0700

    Update description
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'dmg'
-maintainer       'Chef Software, Inc'
+maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
-description      'LWRP to install OS X applications from dmgs'
+description      'Resource for installing macOS applications from DMGs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.1.1'
 supports         'mac_os_x'
