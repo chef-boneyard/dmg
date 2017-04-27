0a96d3277ee9ae4b2dab9c6418750e6ab240ed4c
Tim Smith <tsmith@chef.io>
2017-04-27 11:59:05 -0700

    Add a valid inspec test
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,11 +1,3 @@
-describe command('ps ax | grep activem[q]') do
-  its('exit_status') { should eq 0 }
-end
-
-describe command('/opt/apache-activemq-5.14.4/bin/activemq producer') do
-  its('stdout') { should match(/Produced: 1000 messages/) }
-end
-
-describe command('/opt/apache-activemq-5.14.4/bin/activemq consumer') do
-  its('stdout') { should match(/Consumed: 1000 messages/) }
+describe command('/Applications/Tunnelblick.app/Contents/Resources/openvpnstart') do
+  it { should exist }
 end
